db47f74bd812f3928e3575290ac9cefe97263cf6
Lumenkowski <lumenkowski@gmail.com>
2023-05-07 21:49:47 +0500

    Create a basic scene

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 import "./main.css"
+import * as THREE from "three"
+import { OrbitControls } from "three/addons/controls/OrbitControls"
+import { setup } from "./setup"
+
+const main = () => {
+
+	const { scene, camera, renderer } = setup()
+
+	new OrbitControls( camera, renderer.domElement )
+
+	scene.add( new THREE.AxesHelper( 1000 ) )
+}
+
+main()
---
+++ src/setup.js
@@ -0,0 +1,45 @@
+import * as THREE from "three"
+
+const setup = () => {
+
+	const scene = new THREE.Scene()
+
+	const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 )
+	camera.up = new THREE.Vector3( 0, 0, 1 )
+	camera.position.set( 50, 50, 50 )
+
+	const renderer = new THREE.WebGLRenderer( {
+		alpha: true,
+		antialias: true,
+	} )
+
+	renderer.setPixelRatio( window.devicePixelRatio )
+
+	document.body.insertBefore( renderer.domElement, document.body.firstElementChild )
+
+	const onResize = () => {
+
+		camera.aspect = window.innerWidth / window.innerHeight
+		camera.updateProjectionMatrix()
+
+		renderer.setSize( window.innerWidth, window.innerHeight )
+	}
+
+	onResize()
+
+	window.addEventListener( "resize", onResize, false )
+
+	// default
+
+	renderer.setAnimationLoop( () => renderer.render( scene, camera ) )
+
+	return {
+		scene,
+		camera,
+		renderer,
+	}
+}
+
+export {
+	setup,
+}
