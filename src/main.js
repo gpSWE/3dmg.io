b72d2e7346d9a79760ffa735b4829d73bfa483c8
Lumenkowski <lumenkowski@gmail.com>
2023-05-08 00:48:39 +0500

    Create Box geometry

--- src/main.js
+++ src/main.js
@@ -15,16 +15,49 @@ const main = () => {
 
 	//
 
-	const polygon = geojson.features[ 0 ]
+	const polygon = geojson.features[ 3 ]
 	const centerOfMass = polygon.properties.centerOfMass
 
 	{
 		// PlaneGeometry
 
-		const geometry = utils.createPlaneGeometry( polygon, centerOfMass )
-		const material = new THREE.MeshBasicMaterial( { color: 0x4361ee } )
+		// const geometry = utils.createPlaneGeometry( polygon, centerOfMass )
+		// const material = new THREE.MeshBasicMaterial( { color: 0x4361ee } )
+		// const mesh = new THREE.Mesh( geometry, material )
+		// scene.add( mesh )
+	}
+
+	{
+		// BoxGeometry
+
+		const geometry = utils.createBoxGeometry( polygon, centerOfMass, {
+			elevation: 0,
+			height: 50,
+			face: 1,
+			normal: true,
+			uv: true,
+		} )
+
+		const texture = new THREE.TextureLoader().load( "/uvcheck.jpg" )
+
+		const material = new THREE.MeshBasicMaterial( {
+			map: texture,
+		} )
 		const mesh = new THREE.Mesh( geometry, material )
 		scene.add( mesh )
+	}
+
+	{
+		// FOR TEST
+
+		const texture = new THREE.TextureLoader().load( "/uvcheck.jpg" )
+
+		const geometry = new THREE.BoxGeometry( 100, 100, 100 )
+		const material = new THREE.MeshBasicMaterial( {
+			map: texture,
+		} )
+		const mesh = new THREE.Mesh( geometry, material )
+		// scene.add( mesh )
 	}
 }
 
--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,7 @@ import {
 	Vector3,
 	Box3,
 	BufferGeometry,
+	BufferAttribute,
 	Float32BufferAttribute,
 } from "three"
 
@@ -83,9 +84,219 @@ const createPlaneGeometry = ( polygon, centerOfMass = [], options = { elevation:
 	return geometry
 }
 
+const createBoxGeometry = ( polygon, centerOfMass = [], options = { face: 0, elevation: 0, uv: true, normal: true } ) => {
+
+	const coordinates = polygon.geometry.coordinates
+
+	const data = earcut.flatten( coordinates )
+	const triangles = earcut( data.vertices, data.holes, data.dimensions )
+
+	const flat = coordinates.flat()
+	const vertices = []
+
+	for ( const position of flat ) {
+
+		vertices.push( ...convertTo3DMercator( position, centerOfMass, options.elevation ) )
+	}
+
+	const BASE = new BufferGeometry().setIndex( triangles )
+	BASE.setAttribute( "position", new Float32BufferAttribute( vertices, 3 ) )
+
+	if ( options.uv ) {
+
+		BASE.setAttribute( "uv", new Float32BufferAttribute( generateUV( vertices ), 2 ) )
+	}
+
+	const geometries = [ BASE ]
+
+	const countOfVertices = data.vertices.length / 2
+	const countOfContours = data.holes[ 0 ] || countOfVertices
+
+	// EXTERIOR SIDES
+
+	for ( let i = 0; i < countOfContours - 1; i++ ) {
+
+		const a = new Vector3().fromBufferAttribute( BASE.attributes.position, i )
+		const b = new Vector3().fromBufferAttribute( BASE.attributes.position, i + 1 )
+
+		const c = b.clone().setComponent( 2, options.elevation + options.height )
+		const d = a.clone().setComponent( 2, options.elevation + options.height )
+
+		const EXTERIOR = new BufferGeometry().setIndex( options.face === 0 ? [ 1, 0, 3, 3, 2, 1 ] : [ 2, 3, 0, 0, 1, 2 ] )
+
+		const vertices = [ ...d, ...c, ...b, ...a ]
+
+		if ( options.uv ) {
+
+			EXTERIOR.setAttribute( "uv", new Float32BufferAttribute( generateUV( vertices, 0, 2 ), 2 ) )
+		}
+
+		EXTERIOR.setAttribute( "position", new Float32BufferAttribute( vertices, 3 ) )
+
+		geometries.push( EXTERIOR )
+	}
+
+	// INTERIOR SIDES
+
+	if ( data.holes.length ) {
+
+		const holes = structuredClone( data.holes )
+
+		holes.push( countOfVertices )
+
+		for ( let h = 0; h < holes.length - 1; h++ ) {
+
+			for ( let i = holes[ h ]; i < holes[ h + 1 ] - 1; i++ ) {
+
+				const a = new Vector3().fromBufferAttribute( BASE.attributes.position, i )
+				const b = new Vector3().fromBufferAttribute( BASE.attributes.position, i + 1 )
+
+				const c = b.clone().setComponent( 2, options.elevation + options.height )
+				const d = a.clone().setComponent( 2, options.elevation + options.height )
+
+				const INTERIOR = new BufferGeometry().setIndex( options.face === 1 ? [ 1, 0, 3, 3, 2, 1 ] : [ 2, 3, 0, 0, 1, 2 ] )
+
+				const vertices = [ ...a, ...b, ...c, ...d ]
+
+				if ( options.uv ) {
+
+					INTERIOR.setAttribute( "uv", new Float32BufferAttribute( generateUV( vertices, 0, 2 ), 2 ) )
+				}
+
+				INTERIOR.setAttribute( "position", new Float32BufferAttribute( vertices, 3 ) )
+
+				geometries.push( INTERIOR )
+			}
+		}
+	}
+
+	// TOP
+
+	{
+		const vertices = []
+
+		for ( let i = 0; i < BASE.attributes.position.count; i++ ) {
+
+			vertices.push( ...new Vector3().fromBufferAttribute( BASE.attributes.position, i ).setComponent( 2, options.elevation + options.height ) )
+		}
+
+		const TOP = new BufferGeometry().setIndex( triangles )
+
+		if ( options.uv ) {
+
+			TOP.setAttribute( "uv", BASE.attributes.uv.clone() )
+		}
+
+		TOP.setAttribute( "position", new Float32BufferAttribute( vertices, 3 ) )
+
+		geometries.push( TOP )
+	}
+
+	const geometry = mergeGeometries( geometries )
+
+	if ( options.normal ) {
+
+		geometry.computeVertexNormals()
+	}
+
+	return geometry
+}
+
+const mergeAttributes = attributes => {
+
+	let TypedArray
+	let itemSize
+	let normalized
+	let arrayLength = 0
+
+	for ( let i = 0; i < attributes.length; ++ i ) {
+
+		const attribute = attributes[ i ]
+
+		TypedArray = attribute.array.constructor
+		itemSize = attribute.itemSize
+		normalized = attribute.normalized
+
+		arrayLength += attribute.array.length
+	}
+
+	const array = new TypedArray( arrayLength )
+
+	let offset = 0
+
+	for ( let i = 0; i < attributes.length; ++ i ) {
+
+		array.set( attributes[ i ].array, offset )
+
+		offset += attributes[ i ].array.length
+	}
+
+	return new BufferAttribute( array, itemSize, normalized )
+}
+
+const mergeGeometries = ( geometries, useGroups = false ) => {
+
+	const attributes = {}
+	const mergedGeometry = new BufferGeometry()
+
+	let offset = 0
+
+	for ( let i = 0; i < geometries.length; ++ i ) {
+
+		const geometry = geometries[ i ]
+		let attributesCount = 0
+
+		for ( const name in geometry.attributes ) {
+
+			if ( attributes[ name ] === undefined ) attributes[ name ] = []
+
+			attributes[ name ].push( geometry.attributes[ name ] )
+
+			attributesCount ++
+		}
+	}
+
+	// merge indices
+
+	let indexOffset = 0
+	const mergedIndex = []
+
+	for ( let i = 0; i < geometries.length; ++ i ) {
+
+		const index = geometries[ i ].index
+
+		for ( let j = 0; j < index.count; ++ j ) {
+
+			mergedIndex.push( index.getX( j ) + indexOffset )
+		}
+
+		indexOffset += geometries[ i ].attributes.position.count
+	}
+
+	mergedGeometry.setIndex( mergedIndex )
+
+	// merge attributes
+
+	for ( const name in attributes ) {
+
+		const mergedAttribute = mergeAttributes( attributes[ name ] )
+
+		if ( ! mergedAttribute ) {
+
+			console.error( "error" )
+			return null
+		}
+
+		mergedGeometry.setAttribute( name, mergedAttribute )
+	}
+
+	return mergedGeometry
+}
+
 export {
 	triangulatePolygon,
 	convertTo3DMercator,
 	generateUV,
 	createPlaneGeometry,
+	createBoxGeometry,
 }
